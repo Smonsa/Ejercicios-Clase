b5715f034b9aba2f15a0c75a952286818026561f
Smonsa <82639896+Smonsa@users.noreply.github.com>
2023-02-15 17:01:33 -0400

    Add files via upload

---
+++ Pesos alumnos/js/index.js
@@ -0,0 +1,36 @@
+
+//pesos
+let a =0;
+let b = 0;
+let c =0;
+let d = 0;
+
+const dalea = () => {
+    a++;
+}
+
+const daleb = () => {
+    b++;
+}
+
+const dalec = () => {
+    c++;
+}
+
+const daled = () => {
+    d++;
+}
+
+const simular = () => {
+
+let tot= a+b+c+d;
+
+
+    document.getElementById("aa").value = a;
+    document.getElementById("bb").value = b;
+    document.getElementById("cc").value = c;
+    document.getElementById("dd").value = d;
+    document.getElementById("tota").value = tot;
+    document.getElementById("todosLosResultados").style = "display:block";
+
+}    
